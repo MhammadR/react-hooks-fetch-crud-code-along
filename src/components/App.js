67e39476af1978c079a7718759f0b659dcd92ebb
MhammadR <114111412+MhammadR@users.noreply.github.com>
2022-12-04 22:15:48 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
-import ShoppingList from "./ShoppingList";
-import Header from "./Header";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [url, setUrl] = useState("");
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("https://dog.ceo/api/breeds/image/random");
+      const res = await data.json();
+      setUrl(res.message);
+    };
+    fetchData().catch((error) => console.log("Error:", error));
+  }, []);
 
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList />
+    <div>
+      {url ? <img src={url} alt="A Random Dog" /> : <p>"Loading..." </p>}
     </div>
   );
 }
